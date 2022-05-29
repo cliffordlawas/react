af7c5b0016fc68be8d5165ccec038e2b45bf49d5
Clifford <cliffordl@corebridge.net>
2022-05-29 15:21:00 +0800

    End of Section 5

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,17 +1,28 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css';
+import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
+import ExpenseChart from "./ExpensesChart";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const onFilterChangeData = (expenseFilter) => {
+    setFilteredYear(expenseFilter);
+  };
+  const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear);
+
   return (
     <Card className="expenses">
-      <ExpenseItem
-        amount={props.items[0].amount}
-        title={props.items[0].title}
-        date={props.items[0].date}
+      <ExpensesFilter
+        selected={filteredYear}
+        onFilterChange={onFilterChangeData}
       />
+      <ExpenseChart expenses={filteredExpenses}/>
+     <ExpensesList items={filteredExpenses}/>
     </Card>
   );
-}
+};
 
 export default Expenses;
